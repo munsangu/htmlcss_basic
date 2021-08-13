8f9fea5be6afef667341fb8d97a70a1f183753c5
munsangu <ds-3632@hanmail.net>
2021-08-13 16:36:42 +0900

    20210813

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -1,14 +1,42 @@
+import { useState } from 'react';
 import Button from './Button';
 import Dice from './Dice';
 
+function random(n) {
+  return Math.ceil(Math.random() * n)
+}
+
 function App() {
+  const [num, setNum] = useState(1)
+  const [sum, setSum] = useState(0)
+  const [gameHistory, setGameHistory] = useState([])
+  
+  const handleRollClick = () => {
+    const nextNum = random(6)
+    setNum(nextNum)
+    setSum(sum + nextNum)
+    gameHistory.push(nextNum)
+    setGameHistory([...gameHistory, nextNum])
+  }
+  const handleClearClick = () => {
+    setNum(1)
+    setSum(0)
+    setGameHistory([])
+  }
   return (
     <div>
       <div>
-          <Button>던지기</Button>
-          <Button>처음부터</Button>
+          <Button onClick={handleRollClick}>던지기</Button>
+          <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice/>
+      <div>
+        <h2>나</h2>
+        <Dice color='red' num={num} />
+        <h2>총점</h2>
+        <p>{sum}점</p>
+        <h2>기록</h2>
+        <p>{gameHistory.join(',')}</p>
+      </div>
     </div>
   );
 }
